0da4413345387f5d64734335a2f26cf81afdc3a4
Pablo Rodríguez Flores <prodriguez@localhost.localdomain>
2024-06-03 14:03:59 +0100

    Normalized s3.py and added method to check if a given file exists

--- resources/src/redborder/s3.py
+++ resources/src/redborder/s3.py
@@ -18,13 +18,13 @@
 
 import uuid
 import boto3
+from resources.src.logger.logger import logger
 
 class S3:
-    """
-    Initialize the S3 class with the necessary AWS credentials and bucket information.
-    """
-
     def __init__(self, access_key, secret_key, region_name, bucket_name, endpoint_url):
+        """
+        Initialize the S3 class with the necessary AWS credentials and bucket information.
+        """
         self.s3_client = boto3.client(
             's3',
             aws_access_key_id=access_key,
@@ -33,48 +33,50 @@ class S3:
             endpoint_url=endpoint_url
         )
         self.bucket_name = bucket_name
-    """
-    Upload a local file to an S3 bucket.
 
-    Args:
-        local_file_path (str): The path to the local file you want to upload.
-        s3_key (str): The key under which the file will be stored in S3.
-    """
     def upload_file(self, local_file_path, s3_key):
+        """
+        Upload a local file to an S3 bucket.
+
+        Args:
+            local_file_path (str): The path to the local file you want to upload.
+            s3_key (str): The key under which the file will be stored in S3.
+        """
         try:
             self.s3_client.upload_file(local_file_path, self.bucket_name, s3_key)
-            print(f"Uploaded {local_file_path} to S3")
+            logger.logger.info(f"Uploaded {local_file_path} to S3")
         except Exception as e:
-            print(f"Error uploading file to S3: {str(e)}")
+            logger.logger.error(f"Error uploading file to S3: {str(e)}")
 
-    """
-    Download a file from an S3 bucket to a local directory.
-
-    Args:
-        s3_key (str): The key of the file in S3.
-        local_file_path (str): The local path where the file will be saved.
-    """
     def download_file(self, s3_key, local_file_path):
+        """
+        Download a file from an S3 bucket to a local directory.
+
+        Args:
+            s3_key (str): The key of the file in S3.
+            local_file_path (str): The local path where the file will be saved.
+        """
         try:
             self.s3_client.download_file(self.bucket_name, s3_key, local_file_path)
-            print(f"Downloaded {s3_key} from S3 to {local_file_path}")
+            logger.logger.info(f"Downloaded {s3_key} from S3 to {local_file_path}")
         except Exception as e:
-            print(f"Error downloading file from S3: {str(e)}")
+            logger.logger.error(f"Error downloading file from S3: {str(e)}")
 
-    """
-    List all objects in the S3 bucket.
 
-    This method lists all objects in the S3 bucket and prints their keys.
-    """
     def list_objects(self):
+        """
+        List all objects in the S3 bucket.
+
+        This method lists all objects in the S3 bucket and prints their keys.
+        """
         try:
             response = self.s3_client.list_objects(Bucket=self.bucket_name)
             objects = [obj['Key'] for obj in response.get('Contents', [])]
-            print("Objects in the S3 bucket:")
+            logger.logger.info("Objects in the S3 bucket:")
             for obj in objects:
-                print(obj)
+                logger.logger.info(obj)
         except Exception as e:
-            print(f"Error listing objects in S3 bucket: {str(e)}")
+            logger.logger.error(f"Error listing objects in S3 bucket: {str(e)}")
 
     def list_objects_in_folder(self, folder_prefix):
         """
@@ -87,17 +89,38 @@ class S3:
             objects = [obj['Key'] for obj in response.get('Contents', [])]
             return objects
         except Exception as e:
-            print(f"Error listing objects in S3 folder '{folder_prefix}': {str(e)}")
+            logger.logger.error(f"Error listing objects in S3 folder '{folder_prefix}': {str(e)}")
             return []
-    """
-    Delete an object from the S3 bucket.
 
-    Args:
-        s3_key (str): The key of the object to be deleted.
-    """
     def delete_object(self, s3_key):
+        """
+        Delete an object from the S3 bucket.
+
+        Args:
+            s3_key (str): The key of the object to be deleted.
+        """
         try:
             self.s3_client.delete_object(Bucket=self.bucket_name, Key=s3_key)
-            print(f"Deleted {s3_key} from S3")
+            logger.logger.info(f"Deleted {s3_key} from S3")
         except Exception as e:
-            print(f"Error deleting object from S3: {str(e)}")
+            logger.logger.error(f"Error deleting object from S3: {str(e)}")
+
+    def exists(self, s3_key):
+        """
+        Check if a file exists in the S3 bucket.
+
+        Args:
+            s3_key (str): The key of the file to check in S3.
+
+        Returns:
+            bool: True if the file exists, False otherwise.
+        """
+        try:
+            self.s3_client.head_object(Bucket=self.bucket_name, Key=s3_key)
+            return True
+        except self.s3_client.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                return False
+            else:
+                logger.logger.error(f"Error checking file existence: {str(e)}")
+                return False
